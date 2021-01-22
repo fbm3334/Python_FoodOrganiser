94b3c5a863179abbbc30de9a906bed279b21e95b
Finn Beckitt-Marshall <finn@pictura.co.uk>
2021-01-22 21:00:18 +0000

    Added database initialisation code

--- database.py
+++ database.py
@@ -1,5 +1,18 @@
-# Check whether the database.db file exists - if it does not, then create it
+# Initially open database.db to check that it exists
+f = open('database.db', 'a') # Append
+f.close() # Close straight away
 
-
+# Import sqlite3 to perform database operations
 import sqlite3
 conn = sqlite3.connect('database.db')
+c = conn.cursor() # Database cursor
+
+# Check whether the file is empty
+import os
+if os.stat('database.db').st_size == 0:
+    print('File is empty.')
+    # Initialise the table
+    c.execute('''CREATE TABLE fooddata
+                 (date text, name text, quantity text, amount real, url text)''')
+    conn.commit()
+    print('Created the SQL table.')
