082d7287bd0186292da0b1fc726c8113ee4f3a8e
Finn Beckitt-Marshall <finn@pictura.co.uk>
2021-01-22 21:11:25 +0000

    Created database class

--- database.py
+++ database.py
@@ -1,18 +1,27 @@
-# Initially open database.db to check that it exists
-f = open('database.db', 'a') # Append
-f.close() # Close straight away
+class FoodDatabase:
+    import sqlite3
 
-# Import sqlite3 to perform database operations
-import sqlite3
-conn = sqlite3.connect('database.db')
-c = conn.cursor() # Database cursor
+    def __init__(self):
+        # Initially open database.db to check that it exists
+        f = open('database.db', 'a') # Append
+        f.close() # Close straight away
 
-# Check whether the file is empty
-import os
-if os.stat('database.db').st_size == 0:
-    print('File is empty.')
-    # Initialise the table
-    c.execute('''CREATE TABLE fooddata
-                 (date text, name text, quantity text, amount real, url text)''')
-    conn.commit()
-    print('Created the SQL table.')
+        # Import sqlite3 to perform database operations
+        import sqlite3
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor() # Database cursor
+
+        # Check whether the file is empty
+        import os
+        if os.stat('database.db').st_size == 0:
+            print('File is empty.')
+            # Initialise the table
+            c.execute('''CREATE TABLE fooddata
+                        (date text, name text, quantity text, amount real, url text)''')
+            conn.commit()
+            print('Created the SQL table.')
+        else:
+            print('Database file already exists.')
+        
+        conn.close() # Close the database connection when done
+        
---
+++ main.py
@@ -0,0 +1,2 @@
+from database import FoodDatabase
+fd = FoodDatabase()
