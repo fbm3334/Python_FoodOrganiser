8597a8eba9115f6da31590162b1437940110f643
Finn Beckitt-Marshall <finn@pictura.co.uk>
2021-01-23 11:52:57 +0000

    Added add item function

--- database.py
+++ database.py
@@ -1,3 +1,15 @@
+from dataclasses import dataclass
+import datetime
+
+@dataclass
+class FoodData:
+    barcode: int
+    date: datetime
+    name: str
+    quantity: str
+    amount: int = 1
+    off_url: str = ''
+
 class FoodDatabase:
     import sqlite3
 
@@ -7,8 +19,7 @@ class FoodDatabase:
         f.close() # Close straight away
 
         # Import sqlite3 to perform database operations
-        import sqlite3
-        conn = sqlite3.connect('database.db')
+        conn = self.sqlite3.connect('database.db')
         c = conn.cursor() # Database cursor
 
         # Check whether the file is empty
@@ -17,11 +28,35 @@ class FoodDatabase:
             print('File is empty.')
             # Initialise the table
             c.execute('''CREATE TABLE fooddata
-                        (date text, name text, quantity text, amount real, url text)''')
+                        (barcode integer PRIMARY KEY, date text, name text, quantity text, amount real, url text)''')
             conn.commit()
             print('Created the SQL table.')
         else:
             print('Database file already exists.')
         
         conn.close() # Close the database connection when done
-        
+
+    def add_item(self, item: FoodData):
+        try:
+            # Connect to the database and create the cursor
+            conn = self.sqlite3.connect('database.db')
+            c = conn.cursor() # Database cursor
+            print('Successfully connected to database.')
+            print('Adding the following data - ' + str(item))
+            insert_param = ('''INSERT INTO fooddata
+                        (barcode, date, name, quantity, amount, url)
+                        VALUES (?, ?, ?, ?, ?, ?);''')
+            # Need to convert date/time into a string
+            date_string = item.date.strftime("%d/%m/%Y, %H:%M")
+            data_tuple = (item.barcode, date_string, item.name, item.quantity, item.amount, item.off_url)
+            c.execute(insert_param, data_tuple)
+            conn.commit()
+            print('Item added successfully')
+            c.close()
+        except self.sqlite3.Error as error:
+            print('Failed to insert the item into the SQLite table', error)
+        finally:
+            if conn:
+                conn.close()
+                print('SQLite connection closed')
+
--- main.py
+++ main.py
@@ -1,2 +1,7 @@
-from database import FoodDatabase
+from database import FoodDatabase, FoodData
+import datetime
 fd = FoodDatabase()
+
+item_to_add = FoodData(20303, datetime.datetime(2020, 1, 22), 'Test Item', '300g', 1, 'http://test.com')
+
+fd.add_item(item_to_add)
